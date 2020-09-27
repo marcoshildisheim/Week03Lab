ae081b7f5d0c7343da7c1b1334e6d4a30175af01
820203 <820203@SAIT224250IT.ACDM.DS.SAIT.CA>
2020-09-27 16:37:23 -0600

    Created an Age calculator. User enters age and it will add 1 to their age

---
+++ src/java/servlets/AgeCalculatorServlet.java
@@ -0,0 +1,51 @@
+package servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Marcos Hildisheim 820203
+ */
+public class AgeCalculatorServlet extends HttpServlet 
+{
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException 
+    {
+        getServletContext().getRequestDispatcher("/WEB-INF/agecalculator.jsp")
+                .forward(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException 
+    {
+        String userAge = request.getParameter("age");
+        
+        request.setAttribute("age", userAge);
+        
+        if(userAge== null || userAge.equals(""))
+        {
+            request.setAttribute("message", "Invalid entry. Please enter a valid age. ");
+        }
+        else
+        {
+            try
+            {
+                int newAge = Integer.parseInt(userAge) + 1;
+                request.setAttribute("message", "You will be: " + newAge + "on your next birthday!");
+            }
+            catch (NumberFormatException e)
+            {
+                request.setAttribute("message", "Please enter a number.");
+            }
+        }
+        getServletContext().getRequestDispatcher("/WEB-INF/agecalculator.jsp")
+            .forward(request, response);
+    }
+}
---
+++ web/WEB-INF/agecalculator.jsp
@@ -0,0 +1,23 @@
+<%-- 
+    Document   : agecalculator
+    Created on : Sep 27, 2020, 3:31:00 PM
+    Author     : Marcos Hildisheim 820203
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Age Calculator</title>
+    </head>
+    <body>
+        <h1>The Age Calculator.</h1>
+        <form method="post" action="age">
+            Please enter your age: <input type="text" name="age" value="${age}"><br>
+            <input type="submit" value="Age Next Birthday!">
+        </form>
+            <p>${message}</p><br>
+            <a href="arithmetic">The Arithmetic Calculator</a>
+    </body>
+</html>
